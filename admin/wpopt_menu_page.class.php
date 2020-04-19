49395ce210067024777dd34aa6079c5777d98382
Andrea Frolli <dev.andrea.frolli@outlook.it>
2020-04-20 01:45:08 +0200

    Update wpopt_menu_page.class.php

--- admin/wpopt_menu_page.class.php
+++ admin/wpopt_menu_page.class.php
@@ -17,16 +17,50 @@
 class wpopt_menu_page
 {
 
-    private $option_name = 'wp-opt';
+    private $option_name;
 
     /**
      * Holds the values to be used in the fields callbacks
      */
-    private $options = array();
+    private $options;
 
-    public function __construct()
+    public function __construct($option_name = 'wp-opt')
     {
+        $this->option_name = $option_name;
+
         $this->options = get_option($this->option_name, array());
+
+        add_action('admin_menu', array($this, 'add_plugin_page'));
+
+    }
+
+    public function add_plugin_page()
+    {
+        add_menu_page(
+            'WP Optimizer',
+            'WP Optimizer',
+            'manage_options',
+            'wp-optimizer',
+            array($this, 'render_main'),
+            'dashicons-admin-site'
+        );
+
+        add_submenu_page('wp-optimizer', 'System Info', 'System Info', 'manage_options', 'wpopt-sysinfo', array($this, 'wpopt_info'));
+    }
+
+    public function wpopt_info()
+    {
+        $this->enqueue_scripts();
+
+        require_once WP_OPT_PATH . '/include/wpopt-info.class.php';
+
+        $info = new wpopt_info();
+        $info->render_system_info_page();
+    }
+
+    private function enqueue_scripts()
+    {
+        wp_enqueue_style('wpopto_css', plugin_dir_url(WP_OPT_FILE) . "assets/style.css", array(), '1.0.0');
     }
 
     /**
@@ -36,163 +70,194 @@ class wpopt_menu_page
      */
     public function render_main()
     {
-        if (isset($_POST['clear-db'])) {
+        $this->enqueue_scripts();
+
+        if (isset($_POST['wpopt-action'])) {
+
             if (!check_admin_referer('wpopt-nonce', md5(date("d/m/Y"))))
                 return;
-            $data = wpopt_clear_database();
+
+            $data = array();
+
+            switch ($_POST['wpopt-action']) {
+
+                case 'clear-db':
+                    $data = wpopt_clear_database();
+                    break;
+
+                case 'wpopt-do-cron':
+                    $args = get_option('wp-opt');
+                    if ($args) {
+                        $data = wpopt_do_cron($args);
+                    }
+                    break;
+
+                case 'for-images':
+
+                    /**
+                     * each function will use realpath to ensure path consistency
+                     */
+                    $rel_path = sanitize_text_field($_POST['wp-dir']);
+
+                    if (isset($_POST['opti-all-images'])) {
+
+                        $data = wpopt_optimize_images($rel_path);
+                    }
+                    elseif (isset($_POST['clear-orphimgs'])) {
+
+                        $data = wpopt_clear_orphaned_images($rel_path);
+                    }
+
+                    break;
+            }
         }
-        elseif (isset($_POST['opti-do-cron'])) {
-            if (!check_admin_referer('wpopt-nonce', md5(date("d/m/Y"))))
-                return;
-            $data = wpopt_do_cron(get_option('wp-opt'));
-        }
-        elseif (isset($_POST['opti-all-images'])) {
-            if (!check_admin_referer('wpopt-nonce', md5(date("d/m/Y"))))
-                return;
-            $data = wpopt_optimize_images($_POST['wp-dir']);
-        }
-        elseif (isset($_POST['clear-orphimgs'])) {
-            if (!check_admin_referer('wpopt-nonce', md5(date("d/m/Y"))))
-                return;
-            $data = wpopt_clear_orphaned_images($_POST['wp-dir']);
-        }
+
         settings_errors();
         ?>
-        <div class="wrap">
-            <br class="clearfix">
-            <style>
-                .dn-wrap{margin:auto;display:block;max-width:300px;padding:20px;align-content:center;text-align:center;background:#5a5a5a;color:#fff;-webkit-border-radius:3px;-moz-border-radius:3px;border-radius:3px;-webkit-box-shadow:0 0 8px 2px rgba(0,0,0,.75);-moz-box-shadow:0 0 8px 2px rgba(0,0,0,.75);box-shadow:0 0 8px 2px rgba(0,0,0,.75)}.dn-title{font-size:16px;font-family:sans-serif;font-weight:600}.dn-btc{display:flex;flex:1 1 0}.dn-name{font-weight:600}.dn-value{margin:0 0 0 20px;font-weight:600}.dn-hr{width:100%;background:#0a0a0a;height:1px;margin:22px 0 12px 0}
-            </style>
-            <div class="dn-wrap">
-                <div class="dn-title">Support this project, buy me a coffee.</div>
-                <br>
-                <form action="https://www.paypal.com/cgi-bin/webscr" method="post" target="_top">
-                    <input type="hidden" name="cmd" value="_s-xclick"/>
-                    <input type="hidden" name="hosted_button_id" value="X5ASVPBFLR2JG"/>
-                    <input type="image" src="https://www.paypalobjects.com/en_US/IT/i/btn/btn_donateCC_LG.gif" border="0"
-                           name="submit" title="PayPal - The safer, easier way to pay online!"
-                           alt="Donate with PayPal button"/>
-                    <img alt="" border="0" src="https://www.paypal.com/en_IT/i/scr/pixel.gif" width="1" height="1"/>
-                </form>
-                <div class="dn-hr"></div>
-                <div class="dn-btc"><div class="dn-name">BTC:</div><div class="dn-value">3QE5CyfTxb5kufKxWtx4QEw4qwQyr9J5eo</div></div>
-            </div>
-            <hr>
-            <br class="clearfix">
-            <h1>Optimize your wordpress</h1>
-            <br class="clearfix"><br class="clearfix">
-            <form method="POST">
-                <?php wp_nonce_field('wpopt-nonce', md5(date("d/m/Y"))); ?>
-                <input name="clear-db" type="submit" value="Clear Database"
-                       class="button button-primary button-large">
-            </form>
-            <br class="clearfix"><br class="clearfix">
-            <h2>Select a path in wp-content where the optimization will run</h2>
-            <pre>(is better to use bottom level paths due to high cpu usage)</pre>
-            <form method="POST">
-                <?php wp_nonce_field('wpopt-nonce', md5(date("d/m/Y"))); ?>
-                <input name="wp-dir" type="text"
-                       value="<?php echo date("Y", strtotime('last month')) . '/' . date('m', strtotime('last month')); ?>">
-                <input name="clear-orphimgs" type="submit" value="Clear Orphaned images"
-                       class="button button-primary button-large">
-                <input name="opti-all-images" type="submit" value="Optimize All Images"
-                       class="button button-primary button-large">
-            </form>
-            <br class="clearfix"><br class="clearfix">
-            <form method="POST">
-                <?php wp_nonce_field('wpopt-nonce', md5(date("d/m/Y"))); ?>
-                <input name="opti-do-cron" type="submit" value="Exec Cron-job now"
-                       class="button button-primary button-large">
-            </form>
-            <br class="clearfix"> <br class="clearfix">
-            <hr class="xi-hr">
-            <h1>CronJob setup:</h1>
-            <form method="POST" action="options.php">
-                <input type="hidden" name="<?php echo $this->option_name ?>[change]" value="settings">
-                <?php
-                settings_fields('wp-opt');
-                do_settings_sections('wp-opt');
-                ?>
-                <table>
-                    <tr>
-                        <td>
-                            <div class="xi-text">Auto Clear Time:</div>
-                        </td>
-                        <td>
-                            <input type="time" name="<?php echo $this->option_name ?>[clear-time]" id="clear-time"
-                                   value="<?php echo $this->options['clear-time']; ?>">
-                        </td>
-
-                    </tr>
-                    <tr>
-                        <td>
-                            <div class="xi-text">Active:</div>
-                        </td>
-                        <td>
-                            <input type="checkbox" name="<?php echo $this->option_name ?>[active]" id="active"
-                                   value="1" <?php checked(1, $this->options['active'], true); ?> />
-                        </td>
-                    </tr>
-                    <tr>
-                        <td>
-                            <div class="xi-text">
-                                Auto optimize images
-
-                                (
-                                daily uploads
-
-                                )
-                                :
-                            </div>
-                        </td>
-                        <td>
-                            <input type="checkbox" name="<?php echo $this->option_name ?>[images]"
-                                   id="images"
-                                   value="1" <?php checked(1, $this->options['images'], true); ?> />
-                        </td>
-                    </tr>
-                    <tr>
-                        <td>
-                            <div class="xi-text">
-                                Auto optimize Database:
-                            </div>
-                        </td>
-                        <td>
-                            <input type="checkbox" name="<?php echo $this->option_name ?>[database]" id="database"
-                                   value="1" <?php checked(1, $this->options['database'], true); ?> />
-                        </td>
-                    </tr>
+        <section class="wpopt-wrap">
+            <section class="wpopt">
+                <div class="dn-wrap">
+                    <div class="dn-title">Support this project, buy me a coffee.</div>
                     <br>
-                    <tr>
-                        <td>
-                            <div class="xi-text">Save optimization report:</div>
-                        </td>
-                        <td>
-                            <input type="checkbox" name="<?php echo $this->option_name ?>[save_report]" id="save_report"
-                                   value="1" <?php checked(1, $this->options['save_report'], true); ?> />
-                        </td>
-                    </tr>
-                </table>
+                    <a href="https://www.paypal.me/sh1zen">
+                        <img src="https://www.paypalobjects.com/en_US/IT/i/btn/btn_donateCC_LG.gif"
+                             title="PayPal - The safer, easier way to pay online!" alt="Donate with PayPal button"/>
+                    </a>
+                    <div class="dn-hr"></div>
+                    <div class="dn-btc">
+                        <div class="dn-name">BTC:</div>
+                        <p class="dn-value">3QE5CyfTxb5kufKxWtx4QEw4qwQyr9J5eo</p>
+                    </div>
+                </div>
+            </section>
 
-                <p class="submit">
-                    <input type="submit" class="button-primary" value="<?php _e('Save Changes') ?>"/>
-                </p>
-            </form>
-            <hr class="xi-hr">
-            <h1>Stats:</h1>
-            <?php
-            if (isset($data)) {
-                print_r($data);
-                echo '<hr class="xi-hr">';
-            }
-            ?>
-            <p>
-                <?php
-                echo '<div>peak memory used: ' . $this->convert(memory_get_peak_usage(true)) . '</div><br>';
-                echo '<div>line memory used: ' . $this->convert(memory_get_usage(true)) . '</div><br>';
-                ?>
-            </p>
-        </div>
+            <section class="wpopt">
+                <h1>Optimize your WordPress site:</h1>
+                <block>
+                    <form method="POST">
+                        <?php wp_nonce_field('wpopt-nonce', md5(date("d/m/Y"))); ?>
+                        <input type="hidden" name="wpopt-action" value="clear-db">
+                        <input name="submit" type="submit" value="Clear Database"
+                               class="button button-primary button-large">
+                    </form>
+                </block>
+                <block>
+                    <h2>Select a path in wp-content where the optimization will run</h2>
+                    <pre>(is better to use bottom level paths due to high cpu usage)</pre>
+                    <form method="POST">
+                        <?php wp_nonce_field('wpopt-nonce', md5(date("d/m/Y"))); ?>
+                        <input name="wp-dir" type="text"
+                               value="<?php echo date("Y", strtotime('last month')) . '/' . date('m', strtotime('last month')); ?>">
+                        <input type="hidden" name="wpopt-action" value="for-images">
+                        <input name="clear-orphimgs" type="submit" value="Clear Orphaned images"
+                               class="button button-primary button-large">
+                        <input name="opti-all-images" type="submit" value="Optimize All Images"
+                               class="button button-primary button-large">
+                    </form>
+                </block>
+                <block>
+                    <form method="POST">
+                        <?php wp_nonce_field('wpopt-nonce', md5(date("d/m/Y"))); ?>
+                        <input type="hidden" name="wpopt-action" value="wpopt-do-cron">
+                        <input name="submit" type="submit" value="Exec Cron-job now"
+                               class="button button-primary button-large">
+                    </form>
+                </block>
+                <block>
+                    <h1>Cron-Job setup:</h1>
+                    <form method="POST" action="options.php">
+                        <input type="hidden" name="<?php echo $this->option_name ?>[change]" value="settings">
+                        <?php
+                        settings_fields('wp-opt');
+                        do_settings_sections('wp-opt');
+                        ?>
+                        <table>
+                            <tr>
+                                <td>
+                                    <div class="xi-text">Auto Clear Time:</div>
+                                </td>
+                                <td>
+                                    <input type="time" name="<?php echo $this->option_name ?>[clear-time]"
+                                           id="clear-time"
+                                           value="<?php echo $this->options['clear-time']; ?>">
+                                </td>
+
+                            </tr>
+                            <tr>
+                                <td>
+                                    <div class="xi-text">Active:</div>
+                                </td>
+                                <td>
+                                    <input type="checkbox" name="<?php echo $this->option_name ?>[active]" id="active"
+                                           value="1" <?php checked(1, $this->options['active'], true); ?> />
+                                </td>
+                            </tr>
+                            <tr>
+                                <td>
+                                    <div class="xi-text">
+                                        Auto optimize images
+
+                                        (
+                                        daily uploads
+
+                                        )
+                                        :
+                                    </div>
+                                </td>
+                                <td>
+                                    <input type="checkbox" name="<?php echo $this->option_name ?>[images]"
+                                           id="images"
+                                           value="1" <?php checked(1, $this->options['images'], true); ?> />
+                                </td>
+                            </tr>
+                            <tr>
+                                <td>
+                                    <div class="xi-text">
+                                        Auto optimize Database:
+                                    </div>
+                                </td>
+                                <td>
+                                    <input type="checkbox" name="<?php echo $this->option_name ?>[database]"
+                                           id="database"
+                                           value="1" <?php checked(1, $this->options['database'], true); ?> />
+                                </td>
+                            </tr>
+                            <br>
+                            <tr>
+                                <td>
+                                    <div class="xi-text">Save optimization report:</div>
+                                </td>
+                                <td>
+                                    <input type="checkbox" name="<?php echo $this->option_name ?>[save_report]"
+                                           id="save_report"
+                                           value="1" <?php checked(1, $this->options['save_report'], true); ?> />
+                                </td>
+                            </tr>
+                        </table>
+
+                        <p class="submit">
+                            <input type="submit" class="button-primary" value="<?php _e('Save Changes') ?>"/>
+                        </p>
+                    </form>
+                </block>
+                <block>
+
+                    <h1>Stats:</h1>
+                    <?php
+                    if (isset($data)) {
+                        print_r($data);
+                        echo '<hr class="xi-hr">';
+                    }
+                    ?>
+                    <p>
+                        <?php
+                        echo '<div>peak memory used: ' . $this->convert(memory_get_peak_usage(true)) . '</div><br>';
+                        echo '<div>line memory used: ' . $this->convert(memory_get_usage(true)) . '</div><br>';
+                        ?>
+                    </p>
+                </block>
+            </section>
+        </section>
         <?php
     }
 
