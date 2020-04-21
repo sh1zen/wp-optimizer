519d3de543990664143e1e05c5a5393bb7b00ae1
Andrea Frolli <dev.andrea.frolli@outlook.it>
2020-04-21 18:35:52 +0200

    Update wp-optizer.php

--- wp-optizer.php
+++ wp-optizer.php
@@ -1,5 +1,4 @@
 <?php
-
 /**
  * Plugin Name: WP Optimizer
  * Plugin URI: https://github.com/sh1zen/wp-optimizer
@@ -11,21 +10,18 @@
  * Version: 1.1.0
  */
 
-define('WP_OPT_FILE', __FILE__);
-define('WP_OPT_PATH', dirname(__FILE__));
+define('WPOPT_FILE', __FILE__);
+define('WPOPT_ABSPATH', dirname(__FILE__));
+define('WPOPT_MODULES', dirname(__FILE__) . '/modules');
 
-include_once WP_OPT_PATH . '/include/clear_functions.php';
-include_once WP_OPT_PATH . '/include/functions.php';
+require_once WPOPT_ABSPATH . '/inc/functions.php';
 
-include_once WP_OPT_PATH . '/admin/wpopt_setup.class.php';
-include_once WP_OPT_PATH . '/admin/wpopt_menu_page.class.php';
-
+require_once WPOPT_ABSPATH . '/admin/wpopt.class.php';
 
 /**
  * Starts the plugin.
  *
  * @since 1.0.0
  */
-new wpopt_setup();
-
+wpopt::getInstance();
 
