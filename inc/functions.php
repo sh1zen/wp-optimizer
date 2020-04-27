f781167f2b6346d796ee4c78cc0d539488efcdf9
sh1zen <dev.sh1zen@outlook.it>
2020-04-27 14:39:55 +0200

    Update functions.php

--- inc/functions.php
+++ inc/functions.php
@@ -1,6 +1,5 @@
 <?php
 
-
 function wpopt_convert_size($size)
 {
     $unit = array('b', 'kb', 'mb', 'gb', 'tb', 'pb');
@@ -40,64 +39,34 @@ function wpopt_get_calling_function()
 }
 
 
-function wpopt_do_cron($args = array())
+function wpopt_write_log($log)
 {
-    $start_time = microtime(true);
-    $start_memory = memory_get_usage(true);
-
-    $full_report = array();
-
-    $performer = wpopt_performer::getInstance();
-
-    $default = array(
-        'active'   => false,
-        'images'   => false,
-        'database' => false,
-        'lock'     => false
-    );
-
-    $options = wp_parse_args($args, $default);
-
-    if ((bool)$options['active'] == false)
-        return false;
-
-    if ($options['images']) {
-
-        $images = get_option('wpopt-imgs--todo');
-
-        if (!empty($images)) {
-            $full_report['images'] = $performer->optimize_images($images);
-            update_option('wpopt-imgs--todo', array(), false);
+    if (true === WP_DEBUG) {
+        if (is_array($log) || is_object($log)) {
+            error_log(print_r($log, true));
+        }
+        else {
+            error_log($log);
         }
     }
-
-    if ($options['database'])
-        $full_report['db'] = $performer->clear_database_full();
-
-    $end_time = microtime(true) - $start_time;
-
-    $end_memory = memory_get_usage(true) - $start_memory;
-
-    if ($options['save_report'])
-        file_put_contents(WP_CONTENT_DIR . '/report.opt.txt', wpopt_generate_report($full_report, $start_time, $start_memory), FILE_APPEND);
-
-    return array_merge(array('memory' => wpopt_convert_size($end_memory), 'time' => $end_time), $full_report);
 }
 
-function wpopt_generate_report($data, $start_time = '', $start_memory = '')
+function wpopt_generate_report($data, wpoptTimer $timer = null)
 {
-    $end_time = microtime(true) - $start_time;
-    $end_memory = memory_get_usage(true) - $start_memory;
 
     $report = PHP_EOL . PHP_EOL;
-    $report .= ' Report:' . PHP_EOL . PHP_EOL;
-    $report .= ' Time elapsed: ' . $end_time . PHP_EOL;
-    $report .= ' Memory used: ' . wpopt_convert_size($end_memory) . PHP_EOL;
-    $report .= ' Images cleaned inserted: ' . count($data['images']) . PHP_EOL;
+    $report .= ' ' . __('Report', 'wpopt') . ': ' . PHP_EOL . PHP_EOL;
+
+    if (!is_null($timer)) {
+        $report .= ' ' . __('Time elapsed', 'wpopt') . ': ' . $timer->get_time() . PHP_EOL;
+        $report .= ' ' . __('Memory used', 'wpopt') . ': ' . $timer->get_memory() . PHP_EOL;
+    }
+
+    $report .= ' ' . __('Images cleaned inserted', 'wpopt') . ': ' . count($data['images']) . PHP_EOL;
     $report .= ' ' . $data['db'] . PHP_EOL;
-    $report .= ' Errors: ' . PHP_EOL;
+    $report .= ' ' . __('Errors', 'wpopt') . ': ' . PHP_EOL;
     $report .= ' --------------------------------- ' . PHP_EOL;
-    $report .= ' Number: ' . count($data['errors']) . PHP_EOL;
+    $report .= ' ' . __('Number', 'wpopt') . ': ' . count($data['errors']) . PHP_EOL;
 
     foreach ($data['errors'] as $error)
         $report .= ' - ' . $error . PHP_EOL;
