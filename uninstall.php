22a56c79291167216539ece9d0d1e7438b019762
sh1zen <dev.sh1zen@outlook.it>
2020-04-27 14:42:17 +0200

    Update uninstall.php

--- uninstall.php
+++ uninstall.php
@@ -1,1 +1,29 @@
 <?php
+/**
+ * Uninstall Procedure
+ */
+
+// Make sure that we are uninstalling
+if (!defined('WP_UNINSTALL_PLUGIN'))
+    exit();
+
+// Leave no trail
+$option_names = array('wpopt', 'wpopt-imgs--todo');
+
+if (!is_multisite()) {
+    foreach ($option_names as $option_name)
+        delete_option($option_name);
+}
+else {
+    global $wpdb;
+
+    $blog_ids = $wpdb->get_col("SELECT blog_id FROM $wpdb->blogs");
+    $original_blog_id = get_current_blog_id();
+
+    foreach ($blog_ids as $blog_id) {
+        switch_to_blog($blog_id);
+        foreach ($option_names as $option_name)
+            delete_option($option_name);
+    }
+    switch_to_blog($original_blog_id);
+}
