b76e8f232d12005f8a866c8174aa9fd2ca03ece9
sh1zen <dev.sh1zen@outlook.it>
2020-12-03 23:14:39 +0100

    core improvements

--- inc/WOStorage.class.php
+++ inc/WOStorage.class.php
@@ -1,6 +1,6 @@
 <?php
 /**
- * @package   Flex and Go
+ * @package   wp-optimizer
  * @author    sh1zen
  * @copyright Copyright (C) 2020
  * @license   http://www.gnu.org/licenses/gpl.html GNU/GPL
@@ -32,21 +32,53 @@ class WOStorage
         return self::$_instance;
     }
 
+    public static function generate_identifier($identifier, ...$args)
+    {
+        if (!is_array($identifier) or is_object($identifier))
+            return self::generate_key($identifier, ...$args);
+
+        return 'ID:' . $identifier . '_' . crc32($identifier . serialize($args));
+    }
+
+    public static function generate_key(...$args)
+    {
+        return md5(serialize($args));
+    }
+
     public function disable_autosave()
     {
         remove_action('shutdown', array($this, 'autosave'));
     }
 
-    public function get($key = '', $context = 'default')
+    public function get($key = '', $context = 'default', $blog_id = 0)
     {
-        if (isset($this->cache[$context][$key]))
-            return $this->cache[$context][$key]['data'];
+        $_context = $this->filter_context($context, $blog_id);
 
-        return $this->load($key, $context, true);
+        if (isset($this->cache[$_context][$key]))
+            return $this->cache[$_context][$key]['data'];
+
+        return $this->load($key, $context, true, $blog_id);
     }
 
-    public function load($key, $context = 'default', $cache = false)
+    private function filter_context($context, $_blog_id = 0)
     {
+        $blog_id = '';
+
+        if (is_multisite()) {
+
+            if ($_blog_id)
+                $blog_id = "blog_{$_blog_id}/";
+            else
+                $blog_id = "blog_" . get_current_blog_id() . "/";
+        }
+
+        return $blog_id . $context;
+    }
+
+    public function load($key, $context = 'default', $cache = false, $blog_id = 0)
+    {
+        $context = $this->filter_context($context, $blog_id);
+
         $path = $this->generate_path($context, $key);
 
         if (!file_exists($path))
@@ -57,12 +89,10 @@ class WOStorage
         if (!$data)
             return false;
 
-        if (!($data = json_decode($data, true)))
+        if (!($data = unserialize($data)))
             return false;
 
-        $expire = $data['expire'];
-
-        if (boolval($expire) and $expire < time())
+        if (boolval($data['expire']) and $data['expire'] < time())
             return false;
 
         if ($cache) {
@@ -72,32 +102,48 @@ class WOStorage
         return $data['data'];
     }
 
-    private function generate_path($context = 'default', $key = '')
+    private function generate_path($context, $key = '')
     {
-        if ($context === 'default') {
-            $context = '';
-        }
+        $context = str_replace('default', '', $context);
 
-        return WP_CONTENT_DIR . '/wpopt/storage/' . $context . '/' . $key;
+        return WP_CONTENT_DIR . "/wpopt-storage/{$context}/{$key}";
     }
 
-    public function clear($context = 'default', $key = '')
+    public function delete($context = 'default', $key = '', $blog_id = 0)
     {
+        $this->remove($context, $key, $blog_id);
+
+        $context = $this->filter_context($context, $blog_id);
+
+        $identifier = $key;
+        if ($key and (strpos($key, 'ID:') !== false)) {
+            $key = '';
+        }
+
+        $path = $this->generate_path($context, $key);
+
+        if (!file_exists($path))
+            return false;
+
+        wpopt_delete_files($path, $identifier);
+
+        return true;
+    }
+
+    public function remove($context = 'default', $key = '', $blog_id = 0)
+    {
+        $context = $this->filter_context($context, $blog_id);
+
         if (!empty($key))
             unset($this->cache[$context][$key]);
         else
             unset($this->cache[$context]);
     }
 
-    public function remove($context = 'default', $key = '')
+    public function set($data, $context = 'default', $key = 'main', $expire = false, $force = false, $blog_id = 0)
     {
-        $path = $this->generate_path($context, $key);
+        $context = $this->filter_context($context, $blog_id);
 
-        wpopt_delete_files($path);
-    }
-
-    public function set($data, $context = 'default', $key = 'main', $expire = false, $force = false)
-    {
         // auto add time() to expire if passed just lifespan
         if ($expire <= YEAR_IN_SECONDS) {
             $expire += time();
@@ -106,7 +152,7 @@ class WOStorage
         if (!$force and isset($this->cache[$context][$key])) {
             $force = md5(serialize($data)) !== md5(serialize($this->cache[$context][$key]['data']));
 
-            if(!$force)
+            if (!$force)
                 return;
         }
 
@@ -115,20 +161,28 @@ class WOStorage
             'data'      => $data,
             'file_name' => $key,
             'context'   => $context,
-            'force'     => $force
+            'force'     => $force,
         );
 
         $this->cache[$context][$key] = $args;
     }
 
-    public function get_size($context = '', $key = '')
+    public function get_size($contexts = '', $blog_id = 0)
     {
-        $path = $this->generate_path($context, $key);
+        $size = 0;
+        foreach ((array)$contexts as $context) {
 
-        if(!file_exists($path))
-            return "0 B";
+            $context = $this->filter_context($context, $blog_id);
 
-        return wpopt_bytes2size(wpopt_calc_folder_size($path));
+            $path = $this->generate_path($context);
+
+            if (!file_exists($path))
+                continue;
+
+            $size += wpopt_calc_folder_size($path);
+        }
+
+        return wpopt_bytes2size($size);
     }
 
     public function autosave()
@@ -136,23 +190,37 @@ class WOStorage
         foreach ($this->cache as $context) {
 
             foreach ($context as $element) {
+
                 if ($element['expire'] and $element['expire'] < time())
                     continue;
 
-                $this->save($element);
+                $path = $this->generate_path($element['context']);
+
+                if (file_exists($path . $element['file_name']) and !$element['force'])
+                    continue;
+
+                if (!($cached = serialize($element)))
+                    continue;
+
+                if (!file_exists($path))
+                    mkdir($path, 0777, true);
+
+                file_put_contents($path . $element['file_name'], $cached);
             }
         }
     }
 
-    public function save($args = array())
+    public function save($args = array(), $blog_id = 0)
     {
         $args = array_merge(array(
             'expire'    => false,
             'file_name' => 'main',
             'context'   => 'default',
             'force'     => false,
-            'data'      => array()
+            'data'      => array(),
         ), $args);
+
+        $args['context'] = $this->filter_context($args['context'], $blog_id);
 
         if ($args['expire'] <= YEAR_IN_SECONDS) {
             $args['expire'] += time();
@@ -163,7 +231,7 @@ class WOStorage
         if (file_exists($path . $args['file_name']) and !$args['force'])
             return false;
 
-        $cached = json_encode($args);
+        $cached = serialize($args);
 
         if (!$cached)
             return false;
